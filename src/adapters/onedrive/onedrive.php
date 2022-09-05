4d70d08e9068da684ba434e980e3309c569b0e65
Genilto Vanzin <genilto.vanzin@gmail.com>
2022-09-05 13:54:24 -0300

    First working test. Sending large files

--- src/adapters/onedrive/onedrive.php
+++ src/adapters/onedrive/onedrive.php
@@ -4,41 +4,92 @@
 require_once (__DIR__ . "/vendor/autoload.php");
 
 use Microsoft\Graph\Graph;
+use Microsoft\Graph\Model;
 
 $tenantId = getenv('MS_GRAPH_TENTANT_ID');
 $clientId = getenv('MS_GRAPH_CLIENT_ID');
 $clientSecret = getenv('MS_GRAPH_CLIENT_SECRET');
+$userId = getenv('MS_GRAPH_USER_ID');
 
 $accessToken = getAuthToken ($tenantId, $clientId, $clientSecret);
 
-echo "Access Token: " . $accessToken;
+echo "Access Token: " . $accessToken . "<hr>";
 
 $graph = new Graph();
 $graph->setAccessToken($accessToken);
 
-
+// Upload small file / Single session
 // $graph->createRequest("PUT", "/users/genilto@stonebasyx.com/drive/root/children/teste.txt/content")
-// 	  ->upload( __DIR__ . '/../../../html/toUpload/teste.txt' );
+//  	  ->upload( __DIR__ . '/../../../html/toUpload/teste.txt' );
 
-//$u = new UploadSession();
+$itemPath = "DJI_0002.DNG";
 
-//POST /users/{userId}/drive/items/{itemId}/createUploadSession
-    
+/** @var Model\UploadSession $uploadSession */
+try {
+    $uploadSession = $graph->createRequest("POST", "/users/$userId/drive/root:/$itemPath:/createUploadSession")
+        ->addHeaders(["Content-Type" => "application/json"])
+        // ->attachBody([
+        //     "item" => [
+        //         //"@microsoft.graph.conflictBehavior" => "fail", //"fail (default) | replace | rename",
+        //         "description" => "A large file",
+        //         // "driveItemSource" => [
+        //         //     "@odata.type" => "microsoft.graph.driveItemSource" 
+        //         // ],
+        //         //"fileSize" => 1234,
+        //         //"name" => $itemPath,
+        //         // "mediaSource" => [ 
+        //         //     "@odata.type" => "microsoft.graph.mediaSource" 
+        //         // ]
+        //     ]
+        // ])
+        ->setReturnType(Model\UploadSession::class)
+        ->execute();
+} catch (Exception $e) {
+    die ( $e->getMessage() );
+}
 
-// try {
-//     $sendResult = $graph->createRequest("POST", "/users" . "/" . $this->email->getFrom()->getEmailAddress()->getAddress() . "/sendMail")
-//                 ->attachBody($mailBody)
-//                 ->execute();
-    
-//     echo "<pre>";
-//     print_r($sendResult);
-//     echo "</pre>";
+echo "<pre>";
+print_r($uploadSession);
+echo "</pre>";
 
-// } catch (Exception $e) {
-//     echo "<pre>";
-//     print_r($e);
-//     echo "</pre>";
-// }
+$file = __DIR__ . '/../../../html/toUpload/DJI_0002.DNG';
+$handle = fopen($file, 'r');
+$fileSize = fileSize($file);
+$fileNbByte = $fileSize - 1;
+$chunkSize = 327680 * 3;
+$fgetsLength = $chunkSize + 1;
+$start = 0;
+
+while (!feof($handle)) {
+    echo "Send chunk " . $start . "<br>";
+    $bytes = fread($handle, $fgetsLength);
+    $end = $chunkSize + $start;
+    if ($end > $fileNbByte) {
+        $end = $fileNbByte;
+    }
+    /* or use stream
+    $stream = \GuzzleHttp\Psr7\stream_for($bytes);
+    */
+    try {
+        $res = $graph->createRequest("PUT", $uploadSession->getUploadUrl())
+            ->addHeaders([
+                'Content-Length' => ($end - 1) - $start,
+                'Content-Range' => "bytes " . $start . "-" . $end . "/" . $fileSize
+            ])
+            ->setReturnType(Model\UploadSession::class)
+            ->attachBody($bytes)
+            ->execute();
+
+            echo "<pre>";
+            print_r($res);
+            echo "</pre>";
+
+    } catch (Exception $e) {
+        die ( $e->getMessage() );
+    }
+
+    $start = $end + 1;
+}
 
 function getAuthToken ( $tenantId, $clientId, $clientSecret ) {
     $guzzle = new \GuzzleHttp\Client();
